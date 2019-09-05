6af258b60cb23719880f2e2ad064dad133862e92
laura <lauradondiego@gmail.com>
2019-09-05 14:09:54 -0400

    fixed errors

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 // add this to add environment FIRST UP TOP!
 
 const server = require("./api/server.js");
-const port = process.env.POR || 4000;
-
+const port = process.env.PORT || 4000;
+console.log(process.env.PORT);
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });

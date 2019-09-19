651b16cb770d71bfdebfa05d3d7834e88c03f157
laura <lauradondiego@gmail.com>
2019-09-19 13:57:43 -0400

    adds postgres

--- data/db.js
+++ data/db.js
@@ -1,10 +1,12 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
+// const secrets = require("../config/secrets.js");
+
+const environment = process.env.NODE_ENV || "development";
+const config = require("../knexfile.js")[environment];
 
 // { development: {} }
 // config.development === config['development']
-// both of these are the same 
+// both of these are the same
 
 module.exports = knex(config);
